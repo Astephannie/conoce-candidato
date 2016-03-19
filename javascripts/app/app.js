99976abc063c57e4b4b9498d8be8173288793f06
astephannie <astephannie@gmail.com>
2016-03-19 12:30:37 +0000

    Know what counter and statements

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,1 +1,25 @@
-var jne = angular.module('jne', []);
+var jne = angular.module('jne', ['ngRoute']);
+
+
+jne.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'templates/home.html',
+                controller: 'CandidateController',
+                controllerAs: 'candidateCtrl'
+            })
+            .when('/sabias-que/:candidateId', {
+                templateUrl: '/templates/sabias-que.html',
+                controller: 'KnowWhatController',
+                controllerAs: 'knowwhat'
+            })
+            .when('/resultado', {
+                templateUrl: '/templates/reporte.html'
+            })
+            .otherwise({
+                redirecTo: '/'
+            });
+
+        $locationProvider.html5Mode(true);
+}])
--- javascripts/app/controllers/candidate_controller.js
+++ javascripts/app/controllers/candidate_controller.js
@@ -1,24 +1,76 @@
-jne.controller('CandidateController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
-    $rootScope.candidates = {};
+jne.controller('CandidateController', ['$scope', '$window', '$http', function($scope, $window, $http) {
+    $scope.candidates = {};
     
     $http.get('http://jne-api.herokuapp.com/api/v1/candidates')
     .then(
         function(response) {
-            $rootScope.candidates = response.data.candidates;
+            $scope.candidates = response.data.candidates;
         },
-        function(response) {
-            console.log(response);
+        function(error) {
+            console.log(error);
         });
 }]);
 
-jne.controller('CandidateInfoController', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
-    $scope.get_noticed = function() {
-        $scope.candidates_selected = $rootScope.candidates.filter(function(candidate) {
-            if (candidate.hasOwnProperty('selected')) {
-                return candidate.selected == true;
-            } else {
-                return false;
+jne.controller('KnowWhatController', ['$scope', '$routeParams', '$http', '$location', 
+    function ($scope, $routeParams, $http, $location) {
+        $scope.candidate_id = $routeParams.candidateId;
+        $scope.count = 0;
+        $scope.total = 1;
+        $scope.dont_know_count = 0;
+        $scope.know_count = 0;
+        $scope.dont_matter = 0;
+        $scope.current_statement = "El chisme se hace esperar :)";
+        
+        $http.get('http://jne-api.herokuapp.com/api/v1/candidates_infos/' + $scope.candidate_id)
+        .then(
+            function(response) {
+                $scope.statements = response.data.info;
+                $scope.total = $scope.statements.length;
+                setCurrentStatement();
+            },
+            function(error) {
+                console.log(error);
             }
-        })
+        );
+        
+        $scope.addCounter = function(btn_id) {
+            switch (btn_id) {
+                case 1:
+                    $scope.dont_know_count++;
+                    break;
+                case 2:
+                    $scope.know_count++;
+                    break;
+                case 3:
+                    $scope.dont_matter++;
+                    break;
+                default: 
+                    console.log('Error');
+                    break;
+            }
+            $scope.count++;
+            if ($scope.total == $scope.count) {
+                $location.path('/resultado');
+            }
+            setCurrentStatement();
+        }
+        
+        function getStatementIndex(statements) {
+            available = statements.filter(function(statement) {
+                if (statement.hasOwnProperty("taken")) {
+                    return statement.taken == false;
+                }
+                else {
+                    return true;
+                }
+            })
+             return Math.floor(Math.random()*available.length);
+        }
+        
+        function setCurrentStatement() {
+            $scope.last_statement_index = getStatementIndex($scope.statements);
+            $scope.statements[$scope.last_statement_index].taken = true;
+            $scope.current_statement = $scope.statements[$scope.last_statement_index];
+        }
     }
-}])
+])
