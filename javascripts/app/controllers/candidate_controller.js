2a6e95446231b9df81a7c4ab8f77f49228704f0e
astephannie <astephannie@gmail.com>
2016-03-19 09:51:59 +0000

    Get selected candidates

--- javascripts/app/controllers/candidate_controller.js
+++ javascripts/app/controllers/candidate_controller.js
@@ -1,13 +1,24 @@
-jne.controller('CandidateController', ['$scope', '$http', function($scope, $http) {
-    console.log('mizhaa');
+jne.controller('CandidateController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+    $rootScope.candidates = {};
+    
     $http.get('http://jne-api.herokuapp.com/api/v1/candidates')
     .then(
         function(response) {
-            console.log('mizhita');
-            $scope.candidates = response.data.candidates;
+            $rootScope.candidates = response.data.candidates;
         },
         function(response) {
-            console.log('mizhita pof!');
             console.log(response);
+        });
+}]);
+
+jne.controller('CandidateInfoController', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
+    $scope.get_noticed = function() {
+        $scope.candidates_selected = $rootScope.candidates.filter(function(candidate) {
+            if (candidate.hasOwnProperty('selected')) {
+                return candidate.selected == true;
+            } else {
+                return false;
+            }
         })
+    }
 }])
